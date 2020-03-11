592c3f97150a0389e6593166eaa9cced41e8b3f3
itamarberg <itamar.bergfreund@gmail.com>
2020-03-11 18:54:22 +0200

    created function collect_flight_links()

--- next_page.py
+++ next_page.py
@@ -1,31 +1,42 @@
-"""This code clicks through all the pages"""
+"""
+Module: Data Mining
+Exercise: collecting links from flightstats
+Author: Dor Meir & Itamar Bergfreund
+Date: 11. March 2020
+"""
+
 
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, WebDriverException
 
-# options = Options()
-# options.add_argument("start-maximized")
-# options.add_argument("disable-infobars")
-# options.add_argument("--disable-extensions")
-# driver = webdriver.Chrome(chrome_options=options)
-driver = webdriver.Chrome()
-driver.get('https://www.flightstats.com/v2/flight-tracker/departures/zrh')
+
+def collect_flight_links(url):
+    """This function collects links of each flight from the airport specific web page"""
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    # this loop clicks through the flights by clicking on the next button
+    # and collects all the flight specific links until reaching the last page
+    while True:
+        try:
+            driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"__next\"]/div/section/div/div[2]/div[1]/div[3]/div/div/div[16]/span"))))
+            driver.find_element_by_xpath("//*[@id=\"__next\"]/div/section/div/div[2]/div[1]/div[3]/div/div/div[16]/span").click()
+
+            #todo: get and parse html
+
+        except (TimeoutException, WebDriverException):
+            break
 
 
+def main():
+    url= 'https://www.flightstats.com/v2/flight-tracker/departures/zrh'
+    collect_flight_links(url)
 
-while True:
-    try:
-        driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"__next\"]/div/section/div/div[2]/div[1]/div[3]/div/div/div[16]/span"))))
-        driver.find_element_by_xpath("//*[@id=\"__next\"]/div/section/div/div[2]/div[1]/div[3]/div/div/div[16]/span").click()
 
-        #todo: get and parse html
+if __name__ == "__main__":
+    main()
 
-        # htmprint("Navigating to Next Page")
-
-    except (TimeoutException, WebDriverException) as e:
-        # print("Last page reached")
-        break
