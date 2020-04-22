397c8b534dacb92c1525189f92e088b542bce895
itamarberg <itamar.bergfreund@gmail.com>
2020-04-22 23:49:42 +0300

    quick fix

--- db_feed_flights.py
+++ db_feed_flights.py
@@ -20,9 +20,9 @@ def db_insert_airports():
 
     airports = get_airports()
     db, cur = db_create_cursor()
-    airports.drop(columns=['name'], inplace=True)
+    # airports.drop(columns=['name'], inplace=True)
 
-    query = """INSERT INTO airports (type, elevation_ft, continent, iso_country, iso_region, 
+    query = """INSERT INTO airports (type, name, elevation_ft, continent, iso_country, iso_region, 
             municipality, gps_code, iata_code, local_code, longitude, latitude)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
